37f04f1c09e775891e037d798b1b11f6c9f1424f
Sherman <shc410@gmail.com>
2017-01-26 00:30:37 -0800

    add icons

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <p className='footer'>San Francisco Bay Area, CA, United States</p>
+  	<div>
+    	<p className='footer'>San Francisco Bay Area, CA, United States <a href="https://www.linkedin.com/in/shermanchen410" className="icon-link" target="_blank"><i className="fa fa-linkedin-square icon"></i></a><a href="http://github.com/Sherman-Chen" className='icon-link' target="_blank"><i className="fa fa-github icon"></i></a></p>
+    </div>
   )
 };
 
