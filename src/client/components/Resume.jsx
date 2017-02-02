ac03b813ac054ba2c1c73ecf96bc5d0cfa57892c
Sherman <shc410@gmail.com>
2017-02-01 21:02:43 -0800

    add styles edit reusme component

--- src/client/components/Resume.jsx
+++ src/client/components/Resume.jsx
@@ -33,7 +33,7 @@ const Resume = () => {
           <li><i className="fa fa-code-fork icon"></i><b>Version Control</b>: In event of fire, git checkout -b ahhhhhfire, git add -A (it's an emergency!), git push origin ahhhhhfire, calmly exit building. Git saves lives, frequent commits prevent headaches.</li>
           <li><i className="fa fa-terminal icon"></i><b>Build</b>: Webpack and NPM scripts are cool, have also used Grunt, Gulp, and Bower.</li>
           <li><i className="fa fa-globe icon"></i><b>Deployment</b>: I'm not the most skilled at DevOps, but I'm constantly improving. This site is actively deployed via Heroku, and previous applications were deployed using Heroku, AWS, and Digital Ocean. Docker is pretty neat, containerize everything!</li>
-          <li><i className="fa fa-code icon"></i><b>Languages</b>: JavaScript, HTML, CSS, Python, Ruby, and some Go. I can speak English, and Chinese and am always looking to pick up new languages programming and conversational!</li>
+          <li><i className="fa fa-code icon"></i><b>Languages</b>: JavaScript, HTML, CSS, Python, some Ruby, and some Java. I can speak English, and Chinese and am always looking to pick up new languages programming and conversational!</li>
           <li><i className="fa fa-asterisk icon"></i><b>Others</b>: I also have experience with design tools like Sketch and PhotoShop, and write tests using Mocha, Chai, Jasmine, Karma, and so forth. Have used Continous Integration tools such as CircleCi and TravisCi.</li>
         </ul>
       </section>
